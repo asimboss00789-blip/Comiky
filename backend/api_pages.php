9074e7bd200375aebc041b103e5b4abe1e789793
FallenSoul <asimboss00789@gmail.com>
2025-08-31 06:30:57 +0300

    Update api_pages.php

--- backend/api_pages.php
+++ backend/api_pages.php
@@ -1,1 +1,19 @@
+<?php
+header('Content-Type: application/json');
+require 'ComickIo.php';
 
+$comick = new ComickIo();
+
+if(!isset($_GET['chapter_id'])){
+    echo json_encode(['status'=>false,'message'=>'Chapter ID required']);
+    exit;
+}
+
+$chapterId = $_GET['chapter_id'];
+$pages = $comick->getComicChapter($chapterId);
+
+echo json_encode([
+    'status'=>true,
+    'chapter_id'=>$chapterId,
+    'pages'=>$pages
+]);
